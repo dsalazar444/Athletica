c1dd35dfefcbbf82d78bbf612229012d3353b0f6
Laura <lmarinvelez0@gmail.com>
2026-03-22 17:51:09 -0500

    fix(auth): users endpoint to api

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,6 +6,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('test/', test_serializer),
-    path('auth/register/', RegisterView, name='register'),
-    path('auth/refresh/',  TokenRefreshView.as_view(),     name='token_refresh'),
+    path('api/auth/register/', RegisterView, name='register'),
+    path('api/auth/refresh/',  TokenRefreshView.as_view(),     name='token_refresh'),
 ]
