37e6d8cd7deaf5b15b532f50beed5acbf83923cf
Laura <lmarinvelez0@gmail.com>
2026-04-25 19:19:02 -0500

    fix: format check

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (AthleteProfile, CoachProfile, Goal, User, WeightLog,)
+from .models import AthleteProfile, CoachProfile, Goal, User, WeightLog
 
 
 @admin.register(User)
